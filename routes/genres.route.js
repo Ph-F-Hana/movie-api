9fbd316201f23317e0b312e465bd1a46e2a7ee33
Ph-F-Hana <pl.php77@gmail.com>
2022-01-30 23:50:17 +0200

    init movie-api

---
+++ routes/genres.route.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const validateBody = require('../middlewares/validateBody');
+const validateId = require('../middlewares/validateId');
+const requireAdmin = require('../middlewares/requireAdmin');
+const { createGenreHandler, getAllGenreHandler, getGenreHandler, updateGenreHandler, deleteGenreHandler } = require('../controllers/genres.controller');
+const { createGenreSchema, updateGenreSchema } = require('../schema/genres.schema');
+
+const router = express.Router();
+
+router.get(
+  '/',
+  // requireAdmin,
+  getAllGenreHandler
+);
+
+router.get(
+  '/:genreId',
+  requireAdmin,
+  validateId('genreId'),
+  getGenreHandler
+);
+
+router.post(
+  '/',
+  // requireAdmin,
+  validateBody(createGenreSchema),
+  createGenreHandler
+);
+
+router.put(
+  '/:genreId',
+  // requireAdmin,
+  validateId('genreId'),
+  validateBody(updateGenreSchema),
+  updateGenreHandler
+);
+
+router.delete(
+  '/:genreId',
+  // requireAdmin,
+  validateId('genreId'),
+  deleteGenreHandler
+);
+
+module.exports = router;
---
+++ schema/movie.schema.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+const JoyObjectId = require('joi-objectid');
+const myJoiObjectId = JoyObjectId(Joi);
+
+
+const createMovieSchema = Joi.object({
+  title: Joi.string().min(3).max(200).required(),
+  description: Joi.string().min(5).max(5000).required(),
+  poster: Joi.string().uri().min(10).max(10000).required(),
+  // genres: Joi.array().items(myJoiObjectId()).min(1).required(),
+  genres: Joi.array().items(myJoiObjectId()),
+  rating: Joi.number().required(),
+  director: Joi.string().min(3).max(100).required()
+});
+
+const updateMovieSchema = Joi.object({
+  title: Joi.string().min(3).max(200).required(),
+  description: Joi.string().min(5).max(5000).required(),
+  poster: Joi.string().uri().min(10).max(10000).required(),
+  // genres: Joi.array().items(myJoiObjectId()).min(1).required(),
+  genres: Joi.array().items(myJoiObjectId()),
+  rating: Joi.number().required(),
+  director: Joi.string().min(3).max(100).required()
+});
+
+module.exports = { createMovieSchema, updateMovieSchema };
